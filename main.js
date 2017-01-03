990d56835106093cc06adbecdfe73d81baa1bb4e
sharona1610 <sharona1610@gmail.com>
2017-01-03 10:52:23 +0800

    code for proper positioning of T shape

--- main.js
+++ main.js
@@ -1,49 +1,3 @@
-// function board(){
-//   var y= $('.box').position().top
-//   var x=$('.box').position().left
-//   console.log(x);
-// }
-// board()
-//
-// var tshape = [0,0,0,1,1,1,0,1,0]
-// for(var i=0;i<tshape.length;i++){
-//   if(tshape[i]===1){
-//       $('#t'+(i+1)).css('background','blue')
-//     }
-//   }
-//   console.log(i);
-//   var timeout;
-// //
-// // function timer(){
-// //   timeout =  setInterval(function(){
-// //       var temp= $('.t').position()
-// //       $('.t').css('top', temp.top+10)
-// //   },1000)
-// // }
-// // timer()
-//
-// function angleCalc(){
-//   var angle1 = $('.t').css('-webkit-transform')
-//   console.log(angle1);
-//   if(angle1 !== 'none') {
-//     var values = angle1.split('(')[1].split(')')[0].split(',');
-//     var a = values[0];
-//     var b = values[1];
-//     return angle = Math.round(Math.atan2(b, a) * (180/Math.PI));
-//   }
-//   else{
-//     return angle=0;
-//   }
-// }
-//
-// $('html').keydown(function(x){
-//   if(x.keyCode===38){
-//     var angle = angleCalc();
-//     angle+=90
-//     console.log(angle);
-//     $('.t').css('transform','rotate('+angle+'deg)')
-// }})
-
 
 //Board creation
 var boardArray=[]
@@ -75,45 +29,97 @@ console.log($('#0_0').html());
 var timeout;
 var a=0;
 var b=0;
+var counter = [];
  function timer(){
    timeout =  setInterval(function(){
-     if((a+1)!==19){
-     for (var i = 0; i < tshape.length; i++) {
-       for(var j=0;j<tshape[i].length;j++){
-         $('#'+a+'_'+b).html(tshape[i][j])
-         if(tshape[i][j]===1){
-           $('#'+a+'_'+b).css('background','blue')
+     if((a+1)!==18){
+       for(var j=2;j>=0;j--){
+         var i=2;
+         console.log(($('#'+(a+i+1)+'_'+(b+j)).html()));
+         console.log(a+'+'+i)
+         console.log(b+'+'+j);
+         if((($('#'+(a+i+1)+'_'+(b+j)).html())==='1' && ($('#'+(a+i)+'_'+(b+j)).html())=== '0')
+         || (($('#'+(a+i+1)+'_'+(b+j)).html())==='0')
+          || (($('#'+(a+i+1)+'_'+(b+j)).html())==='0' && ($('#'+(a+i)+'_'+(b+j)).html())==='0')){
+           console.log('entered');
+           counter.push(true)
          }
-         b++
+         else{
+           counter.push(false)
+         }
        }
-       a++
-       b-=3
+       console.log(counter);
+       if(counter.every(checkForCondition)){
+         for (var i = 2; i >=0; i--) {
+           for(var j=2;j>=0;j--){
+            // if(($('#'+(a+i+1)+'_'+(b+j)).html())!=1){
+            //    $('#'+(a+i+1)+'_'+(b+j)).html(tshape[i][j])
+            //    $('#'+(a+i)+'_'+(b+j)).html('0')
+            //    if(tshape[i][j]===1){
+            //      $('#'+(a+i+1)+'_'+(b+j)).css('background','blue')
+            //    }
+             //}
+             if((($('#'+(a+i+1)+'_'+(b+j)).html())==='1')&&(($('#'+(a+i)+'_'+(b+j)).html())==='0')){
+               $('#'+(a+i)+'_'+(b+j)).html('0')
+             }
+             else{
+               $('#'+(a+i+1)+'_'+(b+j)).html(tshape[i][j])
+               $('#'+(a+i)+'_'+(b+j)).html('0')
+             }
+             if(tshape[i][j]===1){
+                 $('#'+(a+i+1)+'_'+(b+j)).css('background','blue')
+               }
+           }
+         }
+       }
+       else{
+         //Reset
+         console.log("Else!!");
+         tshape=[[0,0,0],[1,1,1],[0,1,0]]
+         a=0;
+         b=0;
+         assignShape(tshape)
+         clearInterval(timeout)
+         timer()
+         counter=[]
+       }
+       $('#'+(a+i+1)+'_'+(b)).html(0).css('background','grey')
+       $('#'+(a+i+1)+'_'+(b+1)).html(0).css('background','grey')
+       $('#'+(a+i+1)+'_'+(b+2)).html(0).css('background','grey')
+       $('#'+(a+i+1)+'_'+(b-1)).html(0).css('background','grey')
+       $('#'+(a+i+1)+'_'+(b+3)).html(0).css('background','grey')
+       a+=1;
+
+       function checkForCondition(element){
+         return element===true
+       }
      }
-     a-=4;
-     for(var i=0;i<3;i++){
-       $('#'+a+'_'+b).html(0)
-       $('#'+a+'_'+b).css('background','grey')
-       b++
-     }
-     a+=2
-     b-=3}
      else{
+       tshape=[[0,0,0],[1,1,1],[0,1,0]]
+       a=0;
+       b=0;
+       assignShape(tshape)
        clearInterval(timeout)
+       counter=[]
+       timer()
+       //call new shape
      }
    },1000)
  }
  timer()
+
+
+
  //====================================
 //Rotate
 //=====================================
  $('html').keydown(function(x){
    if(x.keyCode===38){
-     tshape = transpose(tshape)
-     console.log(tshape)
+     tshape = rotate(tshape)
    }
  })
-console.log(tshape);
- function transpose(tshape){
+
+ function rotate(tshape){
    var newArray = []
    for(var i = 0; i < tshape.length; i++){
         newArray.push([]);
@@ -133,16 +139,57 @@ console.log(tshape);
     return(newArray);
 }
 
- function angleCalc(){
-   var angle1 = $('.t').css('-webkit-transform')
-   console.log(angle1);
-   if(angle1 !== 'none') {
-     var values = angle1.split('(')[1].split(')')[0].split(',');
-     var a = values[0];
-     var b = values[1];
-     return angle = Math.round(Math.atan2(b, a) * (180/Math.PI));
-   }
-   else{
-     return angle=0;
-   }
- }
+//=================================
+//Movement
+//=================================
+
+$('html').keydown(function(x){
+  if(x.keyCode===39){
+    b+=1
+    }
+})
+
+$('html').keydown(function(x){
+  if(x.keyCode===37){
+    b-=1
+    }
+})
+
+
+
+
+
+//======
+
+
+      //    ($('#'+(a+i+1)+'_'+(b+j)).html())!==1) {
+      //      counter.push(true)
+      //  }
+      //  else{
+      //    counter.push(false)
+      //  }
+
+      //  if(counter.every(checkForCondition)){
+      //    for (var i = 2; i >=0; i--) {
+      //      for(var j=2;j>=0;j--){
+      //        if(($('#'+(a+i+1)+'_'+(b+j)).html())!=1){
+      //          $('#'+(a+i+1)+'_'+(b+j)).html(tshape[i][j])
+      //          $('#'+(a+i)+'_'+(b+j)).html('0')
+      //         //  console.log(a+i);
+      //         //  console.log(b+j);
+      //          if(tshape[i][j]===1){
+      //            $('#'+(a+i+1)+'_'+(b+j)).css('background','blue')
+      //          }
+      //        }
+      //  }
+      //  for(var i=0;i<3;i++){
+      //    $('#'+a+'_'+b).html(0)
+      //    $('#'+a+'_'+b).css('background','grey')
+      //    b++
+      //  }
+      //  a+=2
+      //  b-=3}
+
+
+
+      // (($('#'+(a+i+1)+'_'+(b+j)).html())===1 && ($('#'+(a+i)+'_'+(b+j)).html())=== 0) ||
